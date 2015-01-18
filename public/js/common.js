fc4c24ebbacf3a656ef6580385667c0754b97b47
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-01-18 19:22:53 +0530

    Make reference API call

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,15 @@
 function ref(id) {
     $("#ref-modal").modal();
+    $.getJSON("reference.php",
+        {
+            "id": id
+        }, function(data) {
+            if(data.sx) {
+                $("#ref-content").html(data.content);
+            } else {
+                $("#ref-content").html("Could not fetch reference! Please try again.")
+            }
+        });
 }
 
 $(document).ready(function() {
